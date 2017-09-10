0907d7fc54ce8a21410b06566dd8a02acd40a94c
himadri <dighehimadri@gmail.com>
2017-09-10 17:11:03 -0400

    this is adding sec ond project in hub

---
+++ src/test/java/himadri/sep9/maven2nd.java
@@ -0,0 +1,17 @@
+package himadri.sep9;
+
+public class maven2nd 
+{
+
+	public abstract class tstclass
+	{
+	public abstract void method3();
+	public abstract void method4();
+	public String name1 = "Himadri";
+	public void method5()
+	{
+		System.out.println("normal method 5 ");
+		
+}
+	}
+}
